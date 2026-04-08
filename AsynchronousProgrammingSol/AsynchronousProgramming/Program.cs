6a6f746f4f332aa8a7c9dda748f53b783b443b28
Sam <samuelgdukes@gmail.com>
2026-04-08 10:51:54 +0100

    Create Hello World async method

---
+++ AsynchronousProgrammingSol/AsynchronousProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AsynchronousProgramming
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Task Started");
+            await HelloWorld();
+            Console.WriteLine("Task Finished");
+        }
+
+        static async Task HelloWorld()
+        {
+            var helloWorld = Task.Run(async () =>
+            {
+                await Task.Delay(2000);
+                Console.WriteLine("Hello World.");
+            });
+
+            await helloWorld;
+        }
+    }
+}
